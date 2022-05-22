05f30769ba981536977e80d4f1c1f1c901f1ee54
zhSHUVO <zhshuvo99@gmail.com>
2022-05-22 13:19:43 +0600

    carousel

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,62 @@
 import React from "react";
+import assemble from "../../assests/images/assemble.jpg";
+import checking from "../../assests/images/checking.jpg";
+import gear from "../../assests/images/gear.jpg";
+import quality from "../../assests/images/quality.jpg";
 import Navbar from "../Universal/Navbar";
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+
+            {/* carousel */}
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={assemble} alt="carouselImg" className="w-full" />{" "}
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">
+                            ❮
+                        </a>
+                        <a href="#slide2" className="btn btn-circle">
+                            ❯
+                        </a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={checking} alt="carouselImg" className="w-full" />{" "}
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">
+                            ❮
+                        </a>
+                        <a href="#slide3" className="btn btn-circle">
+                            ❯
+                        </a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={gear} alt="carouselImg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">
+                            ❮
+                        </a>
+                        <a href="#slide4" className="btn btn-circle">
+                            ❯
+                        </a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src={quality} alt="carouselImg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">
+                            ❮
+                        </a>
+                        <a href="#slide1" className="btn btn-circle">
+                            ❯
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
